aa6c376037b10c7b3f1f39206e3e8d8aee4b28c1
Hardik Rathod <hardikrathod1409@gmail.com>
2022-09-07 21:19:47 +0530

    set local connection

--- db/connect.php
+++ db/connect.php
@@ -1,8 +1,8 @@
 
 
 <?php
-// $con = mysqli_connect("localhost", "root", "", "shiv_fianance") or die("ERROR IN CONNECTION");
-$con = mysqli_connect("localhost", "vdrd6cusg1by" , "ShivFinance@@01" , "shivfinance");
+$con = mysqli_connect("localhost", "root", "", "shiv_fianance") or die("ERROR IN CONNECTION");
+// $con = mysqli_connect("localhost", "vdrd6cusg1by" , "ShivFinance@@01" , "shivfinance");
 
 date_default_timezone_set("Asia/Kolkata");
 $query_date = date("Y-m-d h:i:s");
